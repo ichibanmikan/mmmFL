a7cfc1f7a7926c6300bb916fd9d782a2b189c0b8
ichibanmikan <ichibanmikan@hnu.edu.cn>
2025-02-14 16:39:53 +0800

    model device bug

--- server/global_models/AC_model.py
+++ server/global_models/AC_model.py
@@ -393,6 +393,8 @@ class Tester:
         self.device = device
         
     def test(self):
+        if next(self.model.parameters()).device != self.device:
+            self.model = self.model.to(self.device)
         self.model.eval()
         accs = AverageMeter()
 
