5c5c53aa0064e5ccadab84d02a607259ca202bfc
ichibanmikan <ichibanmikan@hnu.edu.cn>
2025-03-10 15:09:29 +0800

    support for checkpoint-based training resumption

--- server/global_models/USC_model.py
+++ server/global_models/USC_model.py
@@ -129,6 +129,10 @@ class USC:
         self.now_loss = 999
         self.model = MMModel(1, 12)      
         self.device = device
+        self.load_model(
+            os.path.join(
+                os.path.dirname(
+                    os.path.abspath(__file__)), f'models/{self.get_model_name()}.pth'))
         self.model = self.model.to(device)
         
         dl = DataLoader(USC_set(), batch_size=16, num_workers = 4)
@@ -198,6 +202,13 @@ class USC:
         print('==> Saving...')
         torch.save(self.model.cpu().state_dict(), save_file)
         
+    def load_model(self, load_file):
+        if os.path.exists(load_file):
+            print(f'==> Loading model from {load_file}...')
+            self.model.load_state_dict(torch.load(load_file, map_location=self.device), weights_only=True)
+            self.model.to(self.device)
+        else:
+            print(f'==> Model file {load_file} not found. Using initialized model.')        
     
 def accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
